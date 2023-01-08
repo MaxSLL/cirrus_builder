53d146cc25e948291ee8ab7c8ef2d00e8ac3bd1c
MaxSLL <118125531+MaxSLL@users.noreply.github.com>
2023-01-08 22:06:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/donboruza/local_manifests.git --depth 1 -b sweet-13 .repo/local_manifests
+git clone https://github.com/munsuyog/android_device_samsung_m31.git --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_sweet-userdebug
+lunch lineage_m31-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=RooGhz720
 export KBUILD_BUILD_HOST=android-runner
